c3d5a4e3c830e4e7c4bf65f35a05bf33171793e7
Olya Radkovich <olga.radkovich@innowise.com>
2025-10-27 13:51:11 +0100

    Реализация public и private builder'ов для HTTP-клиента

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -1,7 +1,8 @@
 from httpx import Response
-
-from clients.api_client import APIClient
 from typing import TypedDict
+from clients.api_client import APIClient
+
+from clients.private_http_builder import get_private_http_client, AuthenticationUserDict
 
 
 class GetExercisesQueryDict(TypedDict):
@@ -86,3 +87,12 @@ class ExercisesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/exercises/{exercise_id}")
+
+
+def get_exercises_client(user: AuthenticationUserDict) -> ExercisesClient:
+    """
+    Функция создаёт экземпляр ExercisesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию ExercisesClient.
+    """
+    return ExercisesClient(client=get_private_http_client(user))
