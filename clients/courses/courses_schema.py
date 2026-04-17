27e4622f8b488a5309f7542538fcd30aa98096b3
Olya Radkovich <olga.radkovich@innowise.com>
2026-04-17 14:36:51 +0200

    writing API autotests (continued)

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -30,6 +30,12 @@ class GetCoursesQuerySchema(BaseModel):
     user_id: str = Field(alias="userId")
 
 
+class GetCoursesResponseSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    courses: list[CourseSchema]
+
+
 class CreateCourseRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание курса.
@@ -58,8 +64,15 @@ class UpdateCourseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str | None
+    title: str | None = Field(default_factory=fake.sentence)
     max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
     min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
     description: str | None = Field(default_factory=fake.text)
     estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
+    """
+    course: CourseSchema
