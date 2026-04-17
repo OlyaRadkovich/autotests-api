809a57d571193a0960cde9bb530afa0ccd5e25ec
Olya Radkovich <olga.radkovich@innowise.com>
2026-04-17 16:44:32 +0200

    writing API autotests (continued)

--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -1,8 +1,25 @@
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, CourseSchema, \
-    GetCoursesResponseSchema, CreateCourseResponseSchema
+    GetCoursesResponseSchema, CreateCourseResponseSchema, CreateCourseRequestSchema
 from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.files import assert_file
 from tools.assertions.users import assert_user
+
+
+def assert_create_course_response(request: CreateCourseRequestSchema, response: CreateCourseResponseSchema):
+    """
+    Проверяет, что ответ на создание курса соответствует данным из запроса.
+
+    :param request: Исходный запрос на обновление курса.
+    :param response: Ответ API с обновленными данными курса.
+    :raises: AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.course.title, request.title, "title")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
+    assert_equal(response.course.min_score, request.min_score, "min_score")
+    assert_equal(response.course.description, request.description, "description")
+    assert_equal(response.course.estimated_time, request.estimated_time, "estimated_time")
+    assert_equal(response.course.preview_file.id, request.preview_file_id, "preview_file_id")
+    assert_equal(response.course.created_by_user.id, request.created_by_user_id, "created_by_user_id")
 
 
 def assert_update_course_response(request: UpdateCourseRequestSchema, response: UpdateCourseResponseSchema):
@@ -21,6 +38,13 @@ def assert_update_course_response(request: UpdateCourseRequestSchema, response: 
 
 
 def assert_course(actual: CourseSchema, expected: CourseSchema):
+    """
+    Проверяет, что фактические данные курса соответствуют ожидаемым.
+
+    :param actual: Фактические данные курса.
+    :param expected: Ожидаемые данные курса.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
     assert_equal(actual.id, expected.id, "id")
     assert_equal(actual.title, expected.title, "title")
     assert_equal(actual.max_score, expected.max_score, "max_score")
@@ -31,11 +55,20 @@ def assert_course(actual: CourseSchema, expected: CourseSchema):
     assert_file(actual.preview_file, expected.preview_file)
     assert_user(actual.created_by_user, expected.created_by_user)
 
+
 def assert_get_courses_response(
         get_courses_response: GetCoursesResponseSchema,
         create_course_responses: list[CreateCourseResponseSchema]
 ):
-   assert_length(get_courses_response.courses, create_course_responses, "courses")
+    """
+    Проверяет, что ответ на получение списка курсов соответствует ответам на их создание.
 
-   for index, create_course_response in enumerate(create_course_responses):
-       assert_course(get_courses_response.courses[index], create_course_response.course)
+    :param get_courses_response: Ответ API при запросе списка курсов.
+    :param create_course_responses: Список API ответов при создании курсов.
+    :raises AssertionError: Если данные курсов не совпадают.
+    """
+
+    assert_length(get_courses_response.courses, create_course_responses, "courses")
+
+    for index, create_course_response in enumerate(create_course_responses):
+        assert_course(get_courses_response.courses[index], create_course_response.course)
